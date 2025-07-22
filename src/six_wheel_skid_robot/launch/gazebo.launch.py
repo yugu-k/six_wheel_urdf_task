dccb01ac592196e4663f1901606d87e17165261b
Yugandhar Babu K <yugeyugandharbabu@gmail.com>
2025-07-22 11:03:13 +0530

    updated cameras, working on lidars

---
+++ src/six_wheel_skid_robot/launch/gazebo.launch.py
@@ -0,0 +1,79 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def generate_launch_description():
+    urdf_file = PathJoinSubstitution([
+        FindPackageShare('six_wheel_skid_robot'),
+        'urdf',
+        'my_robot.urdf.xacro'
+    ])
+
+    rviz_config_file = PathJoinSubstitution([
+        FindPackageShare('six_wheel_skid_robot'),
+        'rviz',
+        'urdf_config.rviz'
+    ])
+
+    world_file = PathJoinSubstitution([
+        FindPackageShare('six_wheel_skid_robot'),
+        'worlds',
+        'test_world.world'
+    ])
+
+    return LaunchDescription([
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui',
+            output='screen'
+        ),
+
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{
+                'robot_description': Command([
+                    PathJoinSubstitution([FindExecutable(name='xacro')]),
+                    ' ',
+                    urdf_file
+                ])
+            }]
+        ),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                PathJoinSubstitution([
+                    FindPackageShare('gazebo_ros'),
+                    'launch',
+                    'gazebo.launch.py'
+                ])
+            ]),
+            launch_arguments={'world': world_file}.items()
+        ),
+
+        Node(
+            package='gazebo_ros',
+            executable='spawn_entity.py',
+            arguments=[
+                '-topic', 'robot_description',
+                '-entity', 'my_robot'
+            ],
+            output='screen'
+        ),
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=['-d', rviz_config_file]
+        )
+    ])
+
