ede3b26e7b0c5e44bedb9e743d79caed5e5ea4b5
Yugandhar Babu K <yugeyugandharbabu@gmail.com>
2025-07-24 16:25:53 +0530

    New Gazebo World Generated-Also Changed Lidar Configurations

--- src/six_wheel_skid_robot/launch/gazebo.launch.py
+++ src/six_wheel_skid_robot/launch/gazebo.launch.py
@@ -73,7 +73,8 @@ def generate_launch_description():
             executable='rviz2',
             name='rviz2',
             output='screen',
-            arguments=['-d', rviz_config_file]
+            arguments=['-d', rviz_config_file],
+            parameters=[{'use_sim_time': True}]
         )
 
         
